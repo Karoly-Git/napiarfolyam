8cb66d395d0a980a67441eb76bd66ec7022f6e12
Karoly <hornyal.mobile@gmail.com>
2023-06-18 22:18:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+import { allBanks } from './js/data'
+import './css/app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+
+  const [dataDeviza, setDataDeviza] = useState([])
+  const [currentBank, setCurrentBank] = useState('mnb')
+
+  let isLocalServer = !true;
+
+  let url = isLocalServer ? 'http://localhost:8000/deviza' : 'https://napiarfolyam-3e2954a40ab4.herokuapp.com/deviza';
+
+  const fetchData = () => {
+    Axios.get(url)
+      .then(result => {
+        setDataDeviza(result.data);
+      })
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  let sortedData = dataDeviza
+    .filter(item => item.bank === currentBank)
+    .sort((a, b) => a.penznem.localeCompare(b.penznem));
+
+  const handleBankChange = (e) => {
+    const selectedBank = e.target.options[e.target.selectedIndex].getAttribute('id');
+    setCurrentBank(selectedBank);
+  }
+
+  let mnbGbp = dataDeviza.find(
+    item => item.bank === 'mnb' && item.penznem === 'GBP'
   );
+
+  console.log(mnbGbp);
+  return (
+    <div className="App" >
+      <header>
+        <select onChange={handleBankChange}>
+          {allBanks
+            .map((item, index) => (
+              <option key={index} id={item.id}>{item.bank}</option>
+            ))
+          }
+        </select>
+        <div>
+          <h4>
+            MNB - GBP
+          </h4>
+          <h3>
+            {mnbGbp ? mnbGbp.kozep * 1 + ' HUF' : 'Loading...'}
+          </h3>
+        </div>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Pénznem</th>
+            <th>Vétel</th>
+            <th>Közép</th>
+            <th>Eladás</th>
+            <th>Dátum</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {sortedData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.penznem}</td>
+              <td>{item.vetel}</td>
+              <td>{item.kozep}</td>
+              <td>{item.eladas}</td>
+              <td>{item.datum.split(" ")[0]}</td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </div >
+  )
 }
 
-export default App;
+
+
+
+
+
+
